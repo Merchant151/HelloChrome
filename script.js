6e57816ac80b640cf75a51973de3d357c882a645
Merchant151 <joshntram@gmail.com>
2024-06-22 11:23:41 -0300

    not much work

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var dynamicList = document.getElementById('test-list');
+var textBox = document.getElementbyID("in");
 
 function addItem() {
 	 var listItem = document.createElement('li');
 	listItem.textContent = 'Test Item';
-
 	dynamicList.appendChild(listItem);
 
 }
