200707b5a786ad0f5d1c1333cb31957abd68a534
Merchant151 <joshntram@gmail.com>
2024-07-07 10:43:42 -0400

    prompt testing

--- prompt.js
+++ prompt.js
@@ -1,8 +1,12 @@
 
-
+var name = 'key';
+var prom = 'value';
 
 document.getElementById('save').addEventListener("click",function(){
-console.log('memory testing');
+	console.log('memory testing');
+	chrome.storage.local.set({name: prom}).then(() => {
+	console.log("set values")
+	});
 	chrome.storage.local.get(null).then((result) => {
 		console.log(Object.keys(result));
 	});
