8688af897016ae1645af21a483176bc8666d287f
Merchant151 <joshntram@gmail.com>
2024-07-09 19:50:59 -0400

    var and litteral testing

--- prompt.js
+++ prompt.js
@@ -1,5 +1,6 @@
 
 var name = 'key';
+var nameTwo = 'name';
 var prom = 'value';
 var textBox = document.getElementById("textarea");
 var ActiveKey = '';
@@ -10,14 +11,18 @@ document.getElementById('save').addEventListener("click",function(){
 	console.log("set values");
 	});
 	chrome.storage.local.get(null).then((result) => {
-		console.log(Object.keys(result));
+		console.log('atempting to log keys: ' + Object.keys(result));
 		ActiveKey = Object.keys(result);
 	});
 	console.log('Active Key value is: ' + ActiveKey);
-	console.log('textbox value is: ' + textBox.value);
-	chrome.storage.local.get([ActiveKey]).then((result) => {
-		console.log("value: " + result[name]);
+	//console.log('textbox value is: ' + textBox.value); not interested in this information
+	chrome.storage.local.get(ActiveKey).then((result) => {
+		/// in this log i am getting string 'name'
+		console.log("value: " + result[nameTwo]);
 		////I am testing if I can get other key values from the result.
-		console.log('value: ' + result.name);
+		//in this log i am getting literal name 
+		console.log('value: ' + result.name); //'name'
+		//in this log i am getting string 'key' 
+		console.log('value 3: ' + result[name]); //'key'
 	});
 });
