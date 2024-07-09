a1bf5c66d5f479720e265dd6439dd9fab062a0ef
Merchant151 <joshntram@gmail.com>
2024-07-08 20:55:41 -0400

    working some more

--- prompt.js
+++ prompt.js
@@ -1,13 +1,20 @@
 
 var name = 'key';
 var prom = 'value';
+var textBox = document.getElementById("textarea");
+var ActiveKey = '';
 
 document.getElementById('save').addEventListener("click",function(){
 	console.log('memory testing');
-	chrome.storage.local.set({name: prom}).then(() => {
+	chrome.storage.local.set({name: textBox.value}).then(() => {
 	console.log("set values")
 	});
 	chrome.storage.local.get(null).then((result) => {
 		console.log(Object.keys(result));
+		ActiveKey = Object.keys(result);
+	});
+
+	chrome.storage.local.get([ActiveKey]).then((result) => {
+		console.log("value: " + result.key);
 	});
 });
