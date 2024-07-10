5ebe0d09d18060604cba161cf6eed8e733013674
Merchant151 <joshntram@gmail.com>
2024-07-09 20:08:22 -0400

    added saving multiple prompts

--- prompt.js
+++ prompt.js
@@ -1,28 +1,33 @@
 
-var name = 'key';
-var nameTwo = 'name';
-var prom = 'value';
+//var name = 'key';
+//var nameTwo = 'name';
+//var prom = 'value';
 var textBox = document.getElementById("textarea");
-var ActiveKey = '';
+var promptName = document.getElementById('pName');
+//var ActiveKey = '';
+
 
 document.getElementById('save').addEventListener("click",function(){
 	console.log('memory testing');
-	chrome.storage.local.set({key: textBox.value}).then(() => {
+
+	var data = { [promptName.value]:textBox.value }; 
+	chrome.storage.local.set(data).then(() => {
 	console.log("set values");
 	});
+
 	chrome.storage.local.get(null).then((result) => {
 		console.log('atempting to log keys: ' + Object.keys(result));
-		ActiveKey = Object.keys(result);
+		//ActiveKey = Object.keys(result);
 	});
-	console.log('Active Key value is: ' + ActiveKey);
+	//console.log('Active Key value is: ' + ActiveKey);
 	//console.log('textbox value is: ' + textBox.value); not interested in this information
-	chrome.storage.local.get(ActiveKey).then((result) => {
+	//chrome.storage.local.get(ActiveKey).then((result) => {
 		/// in this log i am getting string 'name'
-		console.log("value: " + result[nameTwo]);
+		//console.log("value: " + result[nameTwo]);
 		////I am testing if I can get other key values from the result.
 		//in this log i am getting literal name 
-		console.log('value: ' + result.name); //'name'
+		//console.log('value: ' + result.name); //'name'
 		//in this log i am getting string 'key' 
-		console.log('value 3: ' + result[name]); //'key'
-	});
+		//console.log('value 3: ' + result[name]); //'key'
+	//});
 });
